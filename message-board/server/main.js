30a308b2930aaf14065b98b4400c86a823cae293
Jason Tham <jtham@sbcglobal.net>
2017-04-18 00:43:54 -0500

    added seeding functions, templating

--- message-board/server/main.js
+++ message-board/server/main.js
@@ -1,5 +1,27 @@
 import { Meteor } from 'meteor/meteor';
+import { Sales } from '../api/sales.js';
 
-Meteor.startup(() => {
-  // code to run on server at startup
-});
+ if (Meteor.isServer) {
+  // This code only runs on the server
+    Meteor.startup(() => {
+    // code to run on server at startupconst num = Sales.find().count();
+    const num = Sales.find().count();
+      if (num === 0) {
+        let qty, prodIndex;
+        const productOptions = ['red', 'green', 'blue', 'purple', 'orange'];
+        for (let index = 0; index < 400; index++) {
+          qty = Math.ceil(1 + Math.random() * 20);
+          prodIndex = Math.floor(Math.random() * productOptions.length);
+          
+          Sales.insert({
+            qty,
+            total: 100 * qty,
+            time: new Date(),
+            products: productOptions.slice(0, prodIndex),
+          });
+        }
+      }
+  });
+}
+
+
