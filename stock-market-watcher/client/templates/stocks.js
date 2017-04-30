2188da727e751393e84a3ead64af233a3deb5714
Jason Tham <jtham@sbcglobal.net>
2017-04-30 00:02:53 -0500

    added delete button and functionality

--- stock-market-watcher/client/templates/stocks.js
+++ stock-market-watcher/client/templates/stocks.js
@@ -3,3 +3,9 @@ Meteor.subscribe('stocks');
 Template.stocks.helpers({
     stocks: () => Stocks.find(),
 });
+
+Template.stocks.events({
+    'click .delete' () {
+        Meteor.call('deleteStock', this._id);
+    },
+});
