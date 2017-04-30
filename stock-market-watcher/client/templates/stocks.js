08f9c6a7e8f76aa49227d048d2a8e5d3bd05a6ab
Jason Tham <jtham@sbcglobal.net>
2017-04-30 00:20:46 -0500

    added session variables and trending filters

--- stock-market-watcher/client/templates/stocks.js
+++ stock-market-watcher/client/templates/stocks.js
@@ -1,11 +1,24 @@
 Meteor.subscribe('stocks');
 
 Template.stocks.helpers({
-    stocks: () => Stocks.find(),
+    stocks: () => {
+        let entries;
+        if (Session.get('filter') === 'up') {
+            entries = Stocks.find({ change: { $gt: 0 }});
+        } else if (Session.get('filter') === 'down') {
+            entries = Stocks.find({ change: { $lt: 0 }});
+        } else {
+            entries = Stocks.find();
+        }
+        return entries;
+    },
 });
 
 Template.stocks.events({
     'click .delete' () {
         Meteor.call('deleteStock', this._id);
     },
+    'change #stock-filter' (event) {
+        Session.set('filter', event.target.value);
+    }
 });
